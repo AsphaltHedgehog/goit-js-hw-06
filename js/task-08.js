81ac0eec6fd0937b1f842f84f3480d7902af3f12
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-09-18 15:29:52 +0300

    complete task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+function submitEvent (event) {
+  event.preventDefault();
+  
+  const form = event.target;
+  const formData = new FormData(form);
+  const dataObj = {};
 
+  formData.forEach((el, i) => {
+    dataObj[i] = el;
+  });
+
+  if (!dataObj.email || !dataObj.password) {
+    alert("Заповніть всі поля");
+    return;
+  };
+
+  console.log(dataObj);
+
+  form.reset();
+};
+
+
+const loginForm = document.querySelector(".login-form");
+if (loginForm) {
+  loginForm.addEventListener("submit", submitEvent)
+};
