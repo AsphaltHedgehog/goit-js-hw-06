ce9dc737b484dea61d5ac8a7cf5b6a8ab12559b1
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-09-18 15:52:21 +0300

    complete task-9

--- js/task-09.js
+++ js/task-09.js
@@ -2,4 +2,19 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+const bckGroundColor = document.querySelector('body');
+
+const colorBtn = document.querySelector(".change-color");
+
+const currentColorString = document.querySelector(".color");
+
+colorBtn.addEventListener("click", changeColor);
+
+function changeColor() {
+  const currentRandomColor = getRandomHexColor()
+  bckGroundColor.style.backgroundColor = currentRandomColor;
+  currentColorString.textContent = currentRandomColor;
+};
+
